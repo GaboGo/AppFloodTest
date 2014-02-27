0923f99b80de263a2d496c12bc61239229557c7f
GM <guoming@GMs-Mac-mini.local>
2014-02-27 10:12:23 +0800

    Initial Commit

---
+++ AppFloodTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppFloodTest
+//
+//  Created by GM on 2/27/14.
+//  Copyright (c) 2014 GM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
